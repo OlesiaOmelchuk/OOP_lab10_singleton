856991298584fa5659fc0fd1891afa9b5ab44a09
olesiaomelchuk <olesyao2003@gmail.com>
2022-11-27 21:28:32 +0200

    added basic classes

---
+++ src/main/java/ua/edu/ucu/apps/adapter/task2/TwitterUser.java
@@ -0,0 +1,14 @@
+package ua.edu.ucu.apps.adapter.task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class TwitterUser {
+    private String userMail;
+    private String country;
+    private LocalDate lastActiveTime;
+}
