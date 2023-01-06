044f0f5a01152eb6cf39e5c71a4c400e4f3eb8a0
OlesiaOmelchuk <olesyao2003@gmail.com>
2023-01-06 13:22:25 +0200

    fixed task 3 using extend

--- src/main/java/ua/edu/ucu/apps/adapter/task3/Authorization.java
+++ src/main/java/ua/edu/ucu/apps/adapter/task3/Authorization.java
@@ -1,13 +1,7 @@
 package ua.edu.ucu.apps.adapter.task3;
 
-public class Authorization {
-    private Авторизація auth;
-
-    public Authorization(){
-        auth = new Авторизація();
-    }
-
+public class Authorization extends Авторизація {
     public boolean authorize(Database db){
-        return auth.авторизуватися(db.getDb());
+        return авторизуватися(db);
     }
 }
--- src/main/java/ua/edu/ucu/apps/adapter/task3/Database.java
+++ src/main/java/ua/edu/ucu/apps/adapter/task3/Database.java
@@ -2,19 +2,12 @@ package ua.edu.ucu.apps.adapter.task3;
 
 import lombok.Getter;
 
-public class Database {
-    @Getter
-    private БазаДаних db;
-
-    public Database(){
-        this.db = new БазаДаних();
-    }
-
+public class Database extends БазаДаних{
     public String getUserData(){
-        return db.отриматиДаніКористувача();
+        return отриматиДаніКористувача();
     }
 
     public String  getStaticData(){
-        return db.отриматиСтатистичніДані();
+        return отриматиСтатистичніДані();
     }
 }
